88fdb6d940e437da835734daf918d42484d1ad3c
Patryk Kawecki <patryk.kawecki1108@wp.pl>
2022-02-21 18:48:57 +0100

    Add project files.

---
+++ CurrencyConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CurrencyConverter
+{
+    public class CurrencyConverter
+    {
+        public static string[] GetTags()
+        {
+            string[] currencyTags = new string[] {"eur", "usd", "jpy", "bgn", "czk", "dkk", "gbp", "huf", "ltl", "lvl"
+            , "pln", "ron", "sek", "chf", "nok", "hrk", "rub", "try", "aud", "brl", "cad", "cny", "hkd", "idr", "ils"
+            , "inr", "krw", "mxn", "myr", "nzd", "php", "sgd", "zar"};
+
+            return currencyTags;
+        }
+
+        public float GetValue(string current)
+        {
+            XmlReader xmlReader = XmlReader.Create("C:\\Users\\Patryk\\Desktop\\ATOS\\CurrencyConverter\\XMLFile1.xml");
+            while (xmlReader.Read())
+            {
+
+                if ((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube") && (xmlReader.GetAttribute("currency") == current))
+                {
+                    if (xmlReader.HasAttributes)
+                    {                           
+                        float rate = float.Parse(xmlReader.GetAttribute("rate").Replace('.', ','));
+                        return rate;
+                    }
+                }
+            }
+            return 0; 
+        }
+
+        public float ChangeValue(float val, float amount)
+        {
+            float result = amount * val;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Xml;
+
+namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Euro currency converter");            
+            string[] availableCurrency = CurrencyConverter.GetTags();
+            Console.WriteLine("This is the list of Available Currencies");
+            Console.WriteLine(string.Join(",", availableCurrency).ToUpper());
+            Console.WriteLine("Select the currency you wish to change your EURO");
+            string current = Console.ReadLine().ToUpper();
+
+            var getVal = new CurrencyConverter();
+
+            float val = getVal.GetValue(current);
+            Console.WriteLine("Insert your amount that you want to change");
+            float amount = float.Parse(Console.ReadLine());
+            var result = getVal.ChangeValue(val, amount);
+            Console.WriteLine(result);
+
+
+
+
+
+        }
+    }
+}
